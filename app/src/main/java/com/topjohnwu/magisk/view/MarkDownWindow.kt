8b0295dbda7c271d81d6fa6e3bb18497b4cbae03
Viktor De Pasquale <info@depasquale.wiki>
2020-08-12 18:59:53 +0200

    Fixed crash when setting text to view that has changed
    Uncaught exception has been caught. We got them guys, we got them all.
    Close #3074

--- app/src/main/java/com/topjohnwu/magisk/view/MarkDownWindow.kt
+++ app/src/main/java/com/topjohnwu/magisk/view/MarkDownWindow.kt
@@ -3,6 +3,7 @@ package com.topjohnwu.magisk.view
 import android.content.Context
 import android.text.Spanned
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.TextView
 import androidx.core.text.PrecomputedTextCompat
 import androidx.core.widget.TextViewCompat
@@ -14,19 +15,69 @@ import org.koin.core.KoinComponent
 import org.koin.core.inject
 import timber.log.Timber
 import kotlin.coroutines.coroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class PrecomputedTextSetter : Markwon.TextSetter {
 
-    override fun setText(tv: TextView, text: Spanned, bufferType: TextView.BufferType, onComplete: Runnable) {
+    override fun setText(
+        tv: TextView,
+        text: Spanned,
+        bufferType: TextView.BufferType,
+        onComplete: Runnable
+    ) {
         val scope = tv.tag as? CoroutineScope ?: GlobalScope
-        scope.launch(Dispatchers.Default) {
-            val pre = PrecomputedTextCompat.create(text, TextViewCompat.getTextMetricsParams(tv))
-            tv.post {
-                TextViewCompat.setPrecomputedText(tv, pre)
-                onComplete.run()
-            }
+        scope.launch(Dispatchers.Main) {
+            trySetText(tv, text, onComplete)
         }
     }
+
+    /**
+     * Tries precomputing text and setting it [retryBudget] + 1 times, otherwise gives up. When
+     * giving up the text will be set to empty string for safety. [onComplete] callback will be
+     * called only and only if the view successfully sets the precomputed text.
+     *
+     * It catches the exception that [TextViewCompat.setPrecomputedText] produces when
+     * configurations of precomputed and current view do **not** match.
+     * */
+    private suspend fun trySetText(
+        tv: TextView,
+        text: Spanned,
+        onComplete: Runnable,
+        retryBudget: Int = 3
+    ) {
+        if (retryBudget < 0) {
+            tv.text = ""
+            return
+        }
+
+        val pre = withContext(Dispatchers.Default) {
+            PrecomputedTextCompat.create(text, TextViewCompat.getTextMetricsParams(tv)).also {
+                tv.awaitPost()
+            }
+        }
+
+        kotlin.runCatching {
+            // throws IllegalStateException if the text is incompatible with the view
+            // view might've changed parameters and needs to be recomputed once more
+            TextViewCompat.setPrecomputedText(tv, pre)
+        }.onSuccess {
+            onComplete.run()
+        }.onFailure {
+            trySetText(tv, text, onComplete, retryBudget - 1)
+        }
+    }
+
+    private suspend fun View.awaitPost() = suspendCoroutine<Unit> {
+        val success = post {
+            it.resume(Unit)
+        }
+
+        if (!success) { // looper is exiting, no need to wait, it will be all over soon
+            it.resume(Unit)
+        }
+    }
+
 }
 
 object MarkDownWindow : KoinComponent {
